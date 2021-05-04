6a6a739f9d1de734420735dc4b367db1d977dac9
andreagavardi <andreagavardi89@gmail.com>
2021-05-04 14:03:04 +0200

    inizializzato index e main.js

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+
+
+
+// creare lista ingredienti e assegnare prezzo a ciascuno
+
+//lstenEvent(click) sul pulsante generate
+
+
+
+
+var ingredienti = [
+    ['cheese', '1.5'],
+    ['mostarda', '0.5'],
+    ['formaggio', '1.5'],
+    ['lattuga', '0.25']
+];
+console.log(ingredienti[1][0]);
+
